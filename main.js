654f7c089a9e34c6bedc8b3b313961c06b096afc
mogamoga <mogamoga.g1thu6@gmail.com>
2023-12-26 06:59:20 +0900

    知らないことだらけ 世界と君のこと

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ const vm = {
     mounted() {
         const image = new Image();
         image.onload = () => {
-            const canvas = this.$refs.canvas;
+            const canvas = this.$refs.averageMethodCanvas;
             const context = canvas.getContext("2d");
             canvas.width = image.width;
             canvas.height = image.height;
             context.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas.width, canvas.height);
             const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-            this.monochromeEx(imageData);
+            this.applyAverageMethod(imageData);
             context.putImageData(imageData, 0, 0);
         };
         image.src = "images/clover_days.jpg";
@@ -21,67 +21,42 @@ const vm = {
         // image.src = "images/しもんきん.jpg";
     },
     methods: {
-        monochromeEx(imageData) {
+        applyAverageMethod(imageData) {
             const data = imageData.data;
             for (let i = 0; i < data.length; i += 4) {
                 const red   = data[i];
                 const green = data[i + 1];
                 const blue  = data[i + 2];
-                const min = Math.min(red, green, blue);
-                // const average = (red + green + blue) / 3;
-                // const average = 0.2989 * red + 0.5870 * green + 0.1140 * blue;
-                const average = 0.21 * red + 0.72 * green + 0.07 * blue;
-                data[i]     = average;
-                data[i + 1] = average;
-                data[i + 2] = average;
-                // data[i]     = min;
-                // data[i + 1] = min;
-                // data[i + 2] = min;
-                // data[i]     = 0x00;
-                // data[i + 1] = 0x00;
-                // data[i + 2] = 0x00;
-                // data[i + 3] = 0xFF - average;
-                continue;
-                if (red === green && green === blue) {
-                    data[i]     = average;
-                    data[i + 1] = average;
-                    data[i + 2] = average;
-                }
-                else if (min === red && min === green) {
-                    data[i]     = average;
-                    data[i + 1] = average;
-                    data[i + 2] = 0xFF;
-                }
-                else if (min === red && min === blue) {
-                    data[i]     = average;
-                    data[i + 1] = 0xFF;
-                    data[i + 2] = average;
-                }
-                else if (min === green && min === blue) {
-                    data[i]     = 0xFF;
-                    data[i + 1] = average;
-                    data[i + 2] = average;
-                }
-                else if (min === red) {
-                    data[i]     = average;
-                    data[i + 1] = 0xFF;
-                    data[i + 2] = 0xFF;
-                }
-                else if (min === green) {
-                    data[i]     = 0xFF;
-                    data[i + 1] = average;
-                    data[i + 2] = 0xFF;
-                }
-                else if (min === blue) {
-                    data[i]     = 0xFF;
-                    data[i + 1] = 0xFF;
-                    data[i + 2] = average;
-                }
-                else {
-                    throw new Error("このエラーが発生した場合、分岐に見落としがある。");
-                }
+                const grayscaleValue = (red + green + blue) / 3;
+                data[i]     = grayscaleValue;
+                data[i + 1] = grayscaleValue;
+                data[i + 2] = grayscaleValue;
             }
-        }
+        },
+        applyWeightedAverageMethod(imageData) {
+            const data = imageData.data;
+            for (let i = 0; i < data.length; i += 4) {
+                const red   = data[i];
+                const green = data[i + 1];
+                const blue  = data[i + 2];
+                const grayscaleValue = 0.2989 * red + 0.5870 * green + 0.1140 * blue;
+                data[i]     = grayscaleValue;
+                data[i + 1] = grayscaleValue;
+                data[i + 2] = grayscaleValue;
+            }
+        },
+        applyLuminosityMethod(imageData) {
+            const data = imageData.data;
+            for (let i = 0; i < data.length; i += 4) {
+                const red   = data[i];
+                const green = data[i + 1];
+                const blue  = data[i + 2];
+                const grayscaleValue = 0.21 * red + 0.72 * green + 0.07 * blue;
+                data[i]     = grayscaleValue;
+                data[i + 1] = grayscaleValue;
+                data[i + 2] = grayscaleValue;
+            }
+        },
     }
 };
 
