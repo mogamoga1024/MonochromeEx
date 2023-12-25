772129686ebad1900be5d159b9da6af0c8a4b9fe
mogamoga <mogamoga.g1thu6@gmail.com>
2023-12-25 19:22:41 +0900

    ok

--- main.js
+++ main.js
@@ -15,13 +15,48 @@ const vm = {
             context.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas.width, canvas.height);
             const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
             this.monochromeEx(imageData);
-            // todo
+            context.putImageData(imageData, 0, 0);
         };
         image.src = "images/clover_days.jpg";
     },
     methods: {
         monochromeEx(imageData) {
-            // todo
+            const data = imageData.data;
+            const center = 0xFF / 2;
+            for (let i = 0; i < data.length; i += 4) {
+                const red   = data[i];
+                const green = data[i + 1];
+                const blue  = data[i + 2];
+                const max = Math.max(red, green, blue);
+                if (max < center) {
+                    data[i]     = 0xFF;
+                    data[i + 1] = 0xFF;
+                    data[i + 2] = 0xFF;
+                }
+                else if (red === green && green === blue) {
+                    data[i]     = 0x00;
+                    data[i + 1] = 0x00;
+                    data[i + 2] = 0x00;
+                }
+                else if (max === red) {
+                    data[i]     = 0x00;
+                    data[i + 1] = 0xFF;
+                    data[i + 2] = 0xFF;
+                }
+                else if (max === green) {
+                    data[i]     = 0xFF;
+                    data[i + 1] = 0x00;
+                    data[i + 2] = 0xFF;
+                }
+                else if (max === blue) {
+                    data[i]     = 0xFF;
+                    data[i + 1] = 0xFF;
+                    data[i + 2] = 0x00;
+                }
+                else {
+                    throw new Error("このエラーが発生した場合、分岐に見落としがある。");
+                }
+            }
         }
     }
 };
