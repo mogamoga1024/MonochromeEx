8bdede09229c5899713d4e9e737e0fa23a23d663
mogamoga <mogamoga.g1thu6@gmail.com>
2023-12-25 19:00:44 +0900

    1

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 
-Vue.createApp({
+const vm = {
     data() {
         return {
             hoge: 'Hello Vue!'
         }
     },
     mounted() {
-
+        const image = new Image();
+        image.onload = () => {
+            const canvas = this.$refs.canvas;
+            const context = canvas.getContext("2d");
+            canvas.width = image.width;
+            canvas.height = image.height;
+            context.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas.width, canvas.height);
+            const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+            this.monochromeEx(imageData);
+            // todo
+        };
+        image.src = "images/clover_days.jpg";
     },
-}).mount('#app');
+    methods: {
+        monochromeEx(imageData) {
+            // todo
+        }
+    }
+};
+
+Vue.createApp(vm).mount('#app');
