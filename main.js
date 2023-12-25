cf2df019eb0877bd391710c12cf9a9126aa7d534
mogamoga <mogamoga.g1thu6@gmail.com>
2023-12-25 19:53:43 +0900

    1

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 const vm = {
     data() {
         return {
-            hoge: 'Hello Vue!'
         }
     },
     mounted() {
@@ -17,58 +16,57 @@ const vm = {
             this.monochromeEx(imageData);
             context.putImageData(imageData, 0, 0);
         };
-        image.src = "images/clover_days.jpg";
-        // image.src = "images/2.jpg";
+        // image.src = "images/clover_days.jpg";
+        image.src = "images/2.jpg";
         // image.src = "images/しもんきん.jpg";
     },
     methods: {
         monochromeEx(imageData) {
             const data = imageData.data;
-            const center = 0xFF / 2;
             for (let i = 0; i < data.length; i += 4) {
                 const red   = data[i];
                 const green = data[i + 1];
                 const blue  = data[i + 2];
                 const max = Math.max(red, green, blue);
-                if (max > center) {
-                    data[i]     = 0xFF;
-                    data[i + 1] = 0xFF;
-                    data[i + 2] = 0xFF;
-                }
-                else if (red === green && green === blue) {
-                    data[i]     = 0x00;
-                    data[i + 1] = 0x00;
-                    data[i + 2] = 0x00;
+                const average = (red + green + blue) / 3;
+                data[i]     = average;
+                data[i + 1] = average;
+                data[i + 2] = average;
+                continue;
+                if (red === green && green === blue) {
+                    data[i]     = average;
+                    data[i + 1] = average;
+                    data[i + 2] = average;
                 }
                 else if (max === red && max === green) {
-                    data[i]     = 0x00;
-                    data[i + 1] = 0x00;
+                    data[i]     = average;
+                    data[i + 1] = average;
                     data[i + 2] = 0xFF;
                 }
                 else if (max === red && max === blue) {
-                    data[i]     = 0x00;
+                    data[i]     = average;
                     data[i + 1] = 0xFF;
-                    data[i + 2] = 0x00;
+                    data[i + 2] = average;
                 }
                 else if (max === green && max === blue) {
                     data[i]     = 0xFF;
-                    data[i + 1] = 0x00;
-                    data[i + 2] = 0x00;
+                    data[i + 1] = average;
+                    data[i + 2] = average;
                 }
                 else if (max === red) {
-                    data[i]     = 0x00;
+                    data[i]     = average;
                     data[i + 1] = 0xFF;
                     data[i + 2] = 0xFF;
                 }
                 else if (max === green) {
                     data[i]     = 0xFF;
-                    data[i + 1] = 0x00;
+                    data[i + 1] = average;
                     data[i + 2] = 0xFF;
                 }
                 else if (max === blue) {
                     data[i]     = 0xFF;
                     data[i + 1] = 0xFF;
-                    data[i + 2] = 0x00;
+                    data[i + 2] = average;
                 }
                 else {
                     throw new Error("このエラーが発生した場合、分岐に見落としがある。");
