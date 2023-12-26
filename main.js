e97114fd6a23b9beb1c661a2f11291d2f85ac7ac
mogamoga <mogamoga.g1thu6@gmail.com>
2023-12-26 10:36:39 +0900

    きゅうくらりん

--- main.js
+++ main.js
@@ -7,62 +7,60 @@ const vm = {
                 weightedAverageMethod: true,
                 luminosityMethod: true,
             },
-            shouldDisplayCanvas: {
-                averageMethod: true,
-                weightedAverageMethod: true,
-                luminosityMethod: true,
-            },
-            imageSrc: "",
+            imageSrc: "https://picsum.photos/800/400",
         }
     },
     mounted() {
         this.imageSrc = "images/clover_days.jpg";
         // this.imageSrc = "images/2.jpg";
         // this.imageSrc = "images/しもんきん.jpg";
-        this.onClickApplyButton();
+        this.updateAllCanvas();
     },
     methods: {
-        async onClickApplyButton() {
-            if (this.isSelected.averageMethod) {
-                await this.updateCanvas(
-                    this.$refs.averageMethodCanvas,
-                    this.applyAverageMethod
-                );
-            }
-            if (this.isSelected.weightedAverageMethod) {
-                await this.updateCanvas(
-                    this.$refs.weightedAverageMethodCanvas,
-                    this.applyWeightedAverageMethod
-                );
-            }
-            if (this.isSelected.luminosityMethod) {
-                await this.updateCanvas(
-                    this.$refs.luminosityMethodCanvas,
-                    this.applyLuminosityMethod
-                );
-            }
-            Object.assign(this.shouldDisplayCanvas, this.isSelected);
+        onClickRandomImageButton() {
+            this.imageSrc = "https://picsum.photos/800/400";
+            this.updateAllCanvas();
         },
-        updateCanvas(canvas, applyMethod) {
+        loadImage() {
             const image = new Image();
+            image.setAttribute("crossorigin", "anonymous");
             return new Promise((resolve, reject) => {
                 image.onload = () => {
-                    const context = canvas.getContext("2d");
-                    canvas.width = image.width;
-                    canvas.height = image.height;
-                    canvas.style.maxWidth = `${image.width}px`;
-                    context.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas.width, canvas.height);
-                    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-                    applyMethod(imageData);
-                    context.putImageData(imageData, 0, 0);
-                    resolve();
+                    resolve(image);
                 };
                 image.onerror = e => {
-                    console.error(e);
-                    reject();
+                    reject(e);
                 };
                 image.src = this.imageSrc;
             });
+        },
+        async updateAllCanvas() {
+            const image = await this.loadImage();
+            this.updateCanvas(
+                this.$refs.averageMethodCanvas,
+                image,
+                this.applyAverageMethod
+            );
+            this.updateCanvas(
+                this.$refs.weightedAverageMethodCanvas,
+                image,
+                this.applyAverageMethod
+            );
+            this.updateCanvas(
+                this.$refs.luminosityMethodCanvas,
+                image,
+                this.applyAverageMethod
+            );
+        },
+        updateCanvas(canvas, image, applyMethod) {
+            const context = canvas.getContext("2d", {willReadFrequently: true});
+            canvas.width = image.width;
+            canvas.height = image.height;
+            canvas.style.maxWidth = `${image.width}px`;
+            context.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas.width, canvas.height);
+            const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+            applyMethod(imageData);
+            context.putImageData(imageData, 0, 0);
         },
         applyAverageMethod(imageData) {
             const data = imageData.data;
